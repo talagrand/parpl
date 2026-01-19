97f95e51ef9123a8f3cb5b56ca1597b110ee38f2
Eugene Talagrand <git@talagrand.org>
2026-01-19 14:15:06 -0800

    Benchmark larger inputs, batch parses to minimize overhead

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::unwrap_used)]
 
 use bumpalo::Bump;
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
 // CEL expressions of varying complexity
@@ -19,8 +19,74 @@ const CEL_COMPLEX: &str = r#"
 // Scheme expressions of varying complexity
 const SCHEME_SIMPLE: &str = "(+ 1 2)";
 const SCHEME_NESTED: &str = "(if (> (* 5 2) 8) (max 10 5 20) 0)";
+// Recursive Factorial (using inline self-application pattern) - matches rulesxp
+const SCHEME_FACTORIAL: &str =
+    "((lambda (f x) (f f x)) (lambda (self n) (if (<= n 1) 1 (* n (self self (- n 1))))) 10)";
+
+// 1K sample program - exercises deep nesting, lambdas, quotes, lists
+const SCHEME_1K_SAMPLE: &str = r#"((lambda (f x) (f f x))
+ (lambda (self n)
+   (if (<= n 1)
+       1
+       (* n (self self (- n 1)))))
+ 10)
+
+((lambda (compose)
+   (compose
+    (lambda (x) (* x 2))
+    (lambda (x) (+ x 1))))
+ (lambda (f g)
+   (lambda (x) (f (g x)))))
+
+((lambda (fold lst init f)
+   ((lambda (go)
+      (go go lst init))
+    (lambda (self xs acc)
+      (if (null? xs)
+          acc
+          (self self (cdr xs) (f acc (car xs)))))))
+ '(1 2 3 4 5)
+ 0
+ (lambda (acc x) (+ acc x)))
+
+((lambda (map lst f)
+   ((lambda (go)
+      (go go lst))
+    (lambda (self xs)
+      (if (null? xs)
+          '()
+          (cons (f (car xs)) (self self (cdr xs)))))))
+ '(10 20 30 40 50)
+ (lambda (x) (* x x)))
+
+((lambda (filter lst pred)
+   ((lambda (go)
+      (go go lst))
+    (lambda (self xs)
+      (if (null? xs)
+          '()
+          (if (pred (car xs))
+              (cons (car xs) (self self (cdr xs)))
+              (self self (cdr xs)))))))
+ '(1 2 3 4 5 6 7 8 9 10)
+ (lambda (x) (> x 5)))
+
+((lambda (quicksort)
+   (quicksort quicksort '(3 1 4 1 5 9 2 6 5 3 5)))
+ (lambda (self lst)
+   (if (null? lst)
+       '()
+       ((lambda (pivot rest)
+          (append
+           (self self (filter (lambda (x) (< x pivot)) rest))
+           (cons pivot
+                 (self self (filter (lambda (x) (>= x pivot)) rest)))))
+        (car lst)
+        (cdr lst)))))
+"#;
+
+// Additional parpl-specific test cases
 const SCHEME_LAMBDA: &str = "(lambda (x y) (+ (* x x) (* y y)))";
-const SCHEME_DEFINE: &str = "(define (factorial n) (if (<= n 1) 1 (* n (factorial (- n 1)))))";
 const SCHEME_QUOTE: &str = "'(a b c (d e f) (g (h i)))";
 const SCHEME_COMPLEX: &str = r#"
     (let ((x 10)
@@ -64,49 +130,89 @@ fn bench_scheme_parsing(c: &mut Criterion) {
     let mut group = c.benchmark_group("Scheme Parsing");
 
     group.bench_function("Simple (+ 1 2)", |b| {
-        b.iter(|| {
-            let arena = Bump::new();
-            let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_SIMPLE), &arena);
-        })
+        b.iter_batched_ref(
+            Bump::new,
+            |arena| {
+                let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_SIMPLE), arena);
+            },
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Nested", |b| {
-        b.iter(|| {
-            let arena = Bump::new();
-            let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_NESTED), &arena);
-        })
+        b.iter_batched_ref(
+            Bump::new,
+            |arena| {
+                let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_NESTED), arena);
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("Factorial", |b| {
+        b.iter_batched_ref(
+            Bump::new,
+            |arena| {
+                let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_FACTORIAL), arena);
+            },
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Lambda", |b| {
-        b.iter(|| {
-            let arena = Bump::new();
-            let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_LAMBDA), &arena);
-        })
-    });
-
-    group.bench_function("Define", |b| {
-        b.iter(|| {
-            let arena = Bump::new();
-            let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_DEFINE), &arena);
-        })
+        b.iter_batched_ref(
+            Bump::new,
+            |arena| {
+                let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_LAMBDA), arena);
+            },
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Quote", |b| {
-        b.iter(|| {
-            let arena = Bump::new();
-            let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_QUOTE), &arena);
-        })
+        b.iter_batched_ref(
+            Bump::new,
+            |arena| {
+                let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_QUOTE), arena);
+            },
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Complex", |b| {
-        b.iter(|| {
-            let arena = Bump::new();
-            let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_COMPLEX), &arena);
-        })
+        b.iter_batched_ref(
+            Bump::new,
+            |arena| {
+                let _ = parpl::scheme::samples::scheme::read(black_box(SCHEME_COMPLEX), arena);
+            },
+            BatchSize::SmallInput,
+        )
     });
 
     group.finish();
 }
 
-criterion_group!(benches, bench_cel_parsing, bench_scheme_parsing);
+fn bench_minischeme_parsing(c: &mut Criterion) {
+    let mut group = c.benchmark_group("MiniScheme Parsing");
+
+    group.bench_function("Simple (+ 1 2)", |b| {
+        b.iter(|| parpl::scheme::samples::minischeme::read(black_box(SCHEME_SIMPLE)))
+    });
+
+    group.bench_function("Nested", |b| {
+        b.iter(|| parpl::scheme::samples::minischeme::read(black_box(SCHEME_NESTED)))
+    });
+
+    group.bench_function("Factorial", |b| {
+        b.iter(|| parpl::scheme::samples::minischeme::read(black_box(SCHEME_FACTORIAL)))
+    });
+
+    group.bench_function("1K Sample", |b| {
+        b.iter(|| parpl::scheme::samples::minischeme::read(black_box(SCHEME_1K_SAMPLE)))
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_cel_parsing, bench_scheme_parsing, bench_minischeme_parsing);
 criterion_main!(benches);
