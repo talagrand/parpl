b11012d2dde935c4310bcd3025d3811390dd4c19
Eugene Talagrand <git@talagrand.org>
2026-01-24 11:49:04 -0800

    Fix Fuzzing project and update dependencies

--- fuzz/fuzz_targets/parse_scheme.rs
+++ fuzz/fuzz_targets/parse_scheme.rs
@@ -6,6 +6,6 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let arena = Bump::new();
-        let _ = parpl::scheme::samples::scheme::read(s, &arena);
+        let _ = parpl::scheme::reference::arena::read(s, &arena);
     }
 });
